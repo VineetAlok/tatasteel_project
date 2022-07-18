f201aec1a12f37bff5d9c2081b86f2b9c50c3a36
Vineet <vineetalok03@gmail.com>
2022-07-18 23:49:07 +0530

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:tatasteel_project/homepage.dart';
 import 'package:tatasteel_project/login.dart';
 import 'package:tatasteel_project/register.dart';
 import 'package:tatasteel_project/generator.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:lottie/lottie.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: MyLogin(),
+    home: splashscreen(),
     routes: {
       'generator': (context) => generator(),
       'register': (context) => MyRegister(),
@@ -16,4 +19,21 @@ void main() {
       'login': (context) => MyLogin(),
     },
   ));
+}
+
+class splashscreen extends StatelessWidget {
+  const splashscreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+        splash: Lottie.asset('assets/loadingscreen.json'),
+        splashIconSize: 300,
+        backgroundColor: Colors.white,
+        duration: 3000,
+        splashTransition: SplashTransition.fadeTransition,
+        pageTransitionType: PageTransitionType.leftToRightWithFade,
+        animationDuration: const Duration(seconds: 1),
+        nextScreen: MyLogin());
+  }
 }
